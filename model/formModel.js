9e8be0f29710510cac6144621ec9a31f97c48af4
Brian Schmalz <bschmalz@Brians-MacBook-Air.local>
2017-07-07 21:27:47 -0700

    Changed form schema again to get rid of lat/lng.

--- model/formModel.js
+++ model/formModel.js
@@ -6,7 +6,6 @@ const formSchema = new Schema({
   title: {type: String, required: true},
   description : {type: String, required: true},
   address: {type: String, required: true},
-  latlng: {type: Object, required: true},
   pay : {type: Number, required: true}
 });
 
